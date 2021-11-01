96d8138b12e122b7ac403738bb78f2a467752eca
geekkweeks <zahgreat30@gmail.com>
2021-11-01 10:38:59 +0700

    using ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,12 +11,12 @@ export class CockpitComponent implements OnInit {
   // Sharing data between child and parent directives and components
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   //since using local reference 
   //we dont need these variables anymore
-  // newServerName = '';
-  
-  newServerContent = '';
+  // newServerName = '';  
+  // newServerContent = '';
 
   constructor() {}
 
@@ -39,23 +39,42 @@ export class CockpitComponent implements OnInit {
   // }
 
 
-  //pass param HTMLInputElement
+  // //pass param HTMLInputElement
+  // onAddServer(nameInput: HTMLInputElement){
+  //   console.log(this.serverContentInput);
+  //   // console.log(nameInput);
+  //   // console.log(nameInput.value);
+  //   // this.serverCreated.emit({
+  //   //   serverName: nameInput.value,
+  //   //   serverContent: this.newServerContent
+  //   // })
+  // }
+
+  //  //pass param HTMLInputElement
+  // onAddBluePrint(nameInput: HTMLInputElement) {
+  //   // console.log(nameInput);
+  //   // console.log(nameInput.value);
+  //   // this.blueprintCreated.emit({      
+  //   //   serverName: nameInput.value,
+  //   //   serverContent: this.newServerContent
+  //   // })
+  // }
+
+  //getting access to the template & DOM using @ViewChild
   onAddServer(nameInput: HTMLInputElement){
-    console.log(nameInput);
-    console.log(nameInput.value);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
-   //pass param HTMLInputElement
+  //getting access to the template & DOM using @ViewChild
   onAddBluePrint(nameInput: HTMLInputElement) {
-    console.log(nameInput);
-    console.log(nameInput.value);
+    console.log(this.serverContentInput);
     this.blueprintCreated.emit({      
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
